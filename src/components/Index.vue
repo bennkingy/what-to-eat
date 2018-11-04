5e054cb620277f2a5545636610802b3add115f22
Benn King <bennkingy@gmail.com>
2018-11-04 22:46:06 +0000

    Created Index page of app and added dummy data for meals

---
+++ src/components/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="meal in meals" :key="meal.id">
+      <div class="card-content">
+        <h2 class="indigo-text">{{ meal.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in meal.ingredients" :key="index">
+            <span class="chip">{{ ing }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data(){
+    return{
+      meals: [
+        { title: 'Scrambled eggs on toast', slug: 'eggs-on-toas', ingredients: ['eggs', 'salt', 'milk', 'pepper', 'butter'], id: '1' },
+        { title: 'Curry thing', slug: 'curry-thing', ingredients: ['curry power', 'chicken', 'crem freche', 'brocolli', 'golden rice'], id: '2' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.index{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
+.index h2{
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
+.index .ingredients{
+  margin: 30px auto;
+}
+.index .ingredients li{
+  display: inline-block;
+}
+</style>
