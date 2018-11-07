8eae99546e1c85fdf9ca96272abe92ad0e98f47e
Benn King <bennkingy@gmail.com>
2018-11-07 20:51:18 +0000

    Imported add meal component to the app

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import AddMeal from '@/components/AddMeal'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/add-meal',
+      name: 'AddMeal',
+      component: AddMeal
     }
   ]
 })
